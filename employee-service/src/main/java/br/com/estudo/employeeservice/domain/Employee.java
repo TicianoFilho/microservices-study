80208741b93bfd44ea068780bbb66c7457128c19
TicianoFilho <ticianofilho@gmail.com>
2023-03-11 12:11:13 -0300

    refactor: Employee domain

--- employee-service/src/main/java/br/com/estudo/employeeservice/domain/Employee.java
+++ employee-service/src/main/java/br/com/estudo/employeeservice/domain/Employee.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
@@ -24,4 +25,12 @@ public class Employee {
     private String lastName;
 
     private String email;
+
+    private boolean active;
+
+    private boolean deleted;
+
+    private LocalDateTime createdDate;
+
+    private LocalDateTime updatedDate;
 }
